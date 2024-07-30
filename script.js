78f9780ae4300154c0dc9970b579a7899c1cc472
Servomouse <servomouse@protonmail.com>
2024-07-29 19:04:48 -0600

    Fix buttons

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ function updateAlbumCover(imageUrl) {
 // console.log('Muted:', isMuted);
 // });
 
+const buttonBgLight = "#e4e4e4";
+const buttonBgLightHover = "#0056b3";
+const buttonBgDark = "#444";
+const buttonBgDarkHover = "#0056b3";
+
 function toggleTheme() {
     console.log('toggleTheme()');
     const checkbox = document.getElementById('theme-switcher');
@@ -35,35 +40,35 @@ function toggleTheme() {
     if (checkbox.checked) {
         console.log('setting dark mode');
         buttons.forEach(button => {
-            button.classList.add('fade-out');
-            setTimeout(() => {
-                button.style.backgroundImage = "url('imgs/controls_dark_mode.png')";
-                button.classList.remove('fade-out');
-                button.classList.add('fade-in');
-            }, 150);
-            setTimeout(() => {
-                button.classList.remove('fade-in');
-            }, 300);
+            // button.style.backgroundImage = "url('imgs/controls_dark_mode.png')";
+            button.style.backgroundColor = buttonBgDark;
         });
-        // buttons.style.backgroundImage = "url('imgs/controls_light_mode.png')";
         slider.setAttribute('title', 'Switch to light mode');
         document.body.classList.toggle('dark-theme');
     } else {
         console.log('setting light mode');
         buttons.forEach(button => {
-            button.classList.add('fade-out');
-            setTimeout(() => {
-                button.style.backgroundImage = "url('imgs/controls_light_mode.png')";
-                button.classList.remove('fade-out');
-                button.classList.add('fade-in');
-            }, 150);
-            setTimeout(() => {
-                button.classList.remove('fade-in');
-            }, 300);
+            // button.style.backgroundImage = "url('imgs/controls_light_mode.png')";
+            button.style.backgroundColor = buttonBgLight;
         });
-        // buttons.style.backgroundImage = "url('imgs/controls_dark_mode.png')";
         slider.setAttribute('title', 'Switch to dark mode');
         document.body.classList.toggle('dark-theme');
+    }
+}
+
+function buttonHover(x) {
+    if (document.getElementById('theme-switcher').checked) {    // Dark mode
+        x.style.backgroundColor = buttonBgDarkHover;
+    } else {
+        x.style.backgroundColor = buttonBgLightHover;
+    }
+}
+
+function buttonUnhover(x) {
+    if (document.getElementById('theme-switcher').checked) {    // Dark mode
+        x.style.backgroundColor = buttonBgDark;
+    } else {
+        x.style.backgroundColor = buttonBgLight;
     }
 }
 
